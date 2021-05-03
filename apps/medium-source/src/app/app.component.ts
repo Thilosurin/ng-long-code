3fbfda5369cda9977742dca3ebdf57f65349c7ae
thilosurin <roninchayakorn@gmail.com>
2021-05-03 14:36:46 +0700

    initial commit

---
+++ apps/medium-source/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-long-code-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'medium-source';
+}
